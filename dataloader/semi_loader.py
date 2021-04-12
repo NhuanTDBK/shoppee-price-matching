1bbde93a8db5da7e8f2d8a333f33debc175d249c
steve <nhuan.tran@onemount.com>
2021-04-12 04:46:23 +0000

    Rename train

--- dataloader/semi_loader.py
+++ dataloader/semi_loader.py
@@ -1,28 +1,45 @@
+import numpy as np 
+from typing import Union, List
 import tensorflow as tf 
 
 class TextSemiLoader(tf.keras.utils.Sequence):
-    def __init__(self, X, qclusters, qsize=1000, batch_size=32,neg_size = 5,shuffle=True):
+    def __init__(self, X, qclusters, pool_size=1000, batch_size=32,neg_size = 5,shuffle=True):
+        """
+
+        """
         self.X = X
-        self.qclusters = qclusters
+        self.qclusters = np.array(qclusters)
         self.shuffle = shuffle
-        self.qsize = qsize
+        self.pool_size = pool_size
         self.neg_size = neg_size
         self.batch_size = batch_size
-        self.indexes = np.arange(len(self.X))        
+        self.indexes = np.arange(len(self.qclusters))
+
+        self.nclusters = len(np.unique(self.qclusters))
+
+        self.ohe = tf.keras.utils.to_categorical(self.qclusters, num_classes=self.nclusters, dtype="int")
+        self.cluster_bitmap = self.ohe.T
+
+        self.idx2cluster = {i: self.qclusters[i] for i in self.indexes}
+        
+
 
     def __len__(self):
-        return math.ceil(len(self.X) / self.batch_size)
+        return math.ceil(len(self.indexes) / self.batch_size)
 
     def __getitem__(self, idx):
         batch_x_idxs = self.indexes[idx * self.batch_size:(idx + 1) *self.batch_size]
+        for idx in batch_x_idxs:
+            cluster_idx = self.idx2cluster[idx]
+            y_cluster = self.ohe[cluster_idx]
+
+
+        
         # batch_y = self.y[idx * self.batch_size:(idx + 1) *self.batch_size]
 
+    # def on_epoch_end(self):
+    #     'Updates indexes after each epoch'
+    #     if self.shuffle == True:
+    #         np.random.shuffle(self.indexes)
 
-        # return np.array([
-        #     resize(imread(file_name), (200, 200))
-        #         for file_name in batch_x]), np.array(batch_y)
-
-    def on_epoch_end(self):
-        'Updates indexes after each epoch'
-        if self.shuffle == True:
-            np.random.shuffle(self.indexes)
+    
