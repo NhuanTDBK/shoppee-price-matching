10522b7b390e1417be452e3dac67c09b24c32a21
Nhuan Tran <nhuan.tran@onemount.com>
2021-04-14 16:35:30 +0700

    Add tensorboard logging function

--- xlm_roberta_pairwise_random.py
+++ xlm_roberta_pairwise_random.py
@@ -1,38 +1,42 @@
-import os
+import datetime
 import gc
-import time
+import logging
+import os
 from typing import Union
 
-import transformers
-import tensorflow as tf
 import numpy as np
 import pandas as pd
-
+import transformers
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import KFold, train_test_split
-from transformers import BertTokenizer, TFBertModel
 
 from dataloader.semi_loader import RandomTextSemiLoader
 from features.pool import BertLastHiddenState
-from modelling.loss import contrastive_loss
 from modelling.dist import pairwise_dist
+from modelling.loss import contrastive_loss
 from modelling.pooling import *
-
 
 params = {
     "N_CLASSES": 11014,
     "MAX_LEN": 70,
     "MODEL_NAME": 'xlm-roberta-base',
-    "POOLING": "global_avg_1d",
     "EPOCHS": 5,
     "BATCH_SIZE": 16,
-    # "METRIC": "adacos",
-    "LAST_HIDDEN_STATES": 3 
+    "LAST_HIDDEN_STATES": 3,
+    "DRIVE_PATH": "/content/drive/MyDrive/shopee-price"
 }
 
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
-logger = tf.compat.v1.logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("n")
 
+config = transformers.XLMRobertaConfig.from_pretrained(params["MODEL_NAME"])
+config.output_hidden_states = True
+tokenizer = transformers.XLMRobertaTokenizer.from_pretrained(params["MODEL_NAME"])
+
+model_dir = os.path.join(params["DRIVE_PATH"], "saved", "pair-%s" % params["MODEL_NAME"])
+os.makedirs(model_dir, exist_ok=True)
 
 
 def encoder(titles: Union[str]):
@@ -58,103 +62,130 @@ def encoder(titles: Union[str]):
     return input_ids, att_masks, token_type_ids
 
 
-df = pd.read_csv("train.csv")
-df["label"] = LabelEncoder().fit_transform(df["label_group"].tolist())
-X_title = df["title"].to_numpy()
+def create_tf_summary_writer():
+    tb_log_dir = os.path.join(params["DRIVE_PATH"], "logs")
+
+    os.makedirs(tb_log_dir, exist_ok=True)
+
+    current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    train_log_dir = os.path.join(tb_log_dir, current_time, 'train')
+    val_log_dir = os.path.join(tb_log_dir, current_time, 'val')
+
+    train_summary_writer = tf.summary.create_file_writer(train_log_dir)
+    val_summary_writer = tf.summary.create_file_writer(val_log_dir)
+
+    return train_summary_writer, val_summary_writer
 
 
+def create_checkpoint(model, optimizer):
+    checkpoint_prefix = os.path.join(params["DRIVE_PATH"], "tmp/training_checkpoints", params["MODEL_NAME"], "ckpt")
+
+    # Create a Checkpoint that will manage two objects with trackable state,
+    # one we name "optimizer" and the other we name "model".
+    checkpoint = tf.train.Checkpoint(optimizer=optimizer, model=model)
+    return checkpoint, checkpoint_prefix
 
 
-config = transformers.BertConfig.from_pretrained(params["MODEL_NAME"])
-config.output_hidden_states = True
-word_model = transformers.TFAutoModel.from_pretrained(params["MODEL_NAME"],config=config)
-tokenizer = transformers.AutoTokenizer.from_pretrained(params["MODEL_NAME"])
+def create_model():
+    word_model = transformers.TFAutoModel.from_pretrained(params["MODEL_NAME"], config=config)
+
+    ids = tf.keras.layers.Input((params["MAX_LEN"],), dtype=tf.int32)
+    att = tf.keras.layers.Input((params["MAX_LEN"],), dtype=tf.int32)
+    tok = tf.keras.layers.Input((params["MAX_LEN"],), dtype=tf.int32)
+    x1 = word_model(ids, attention_mask=att, token_type_ids=tok)[-1]
+    embedding = BertLastHiddenState(multi_sample_dropout=True)(x1)
+    embedding_norm = tf.math.l2_normalize(embedding, axis=1)
+
+    model = tf.keras.models.Model(inputs=[ids, att, tok], outputs=[embedding_norm])
+
+    optimizer = tf.keras.optimizers.Adam()
+    loss_fn = contrastive_loss
+
+    model.compile(optimizer=optimizer, loss=loss_fn)
+
+    print(model.summary())
+
+    return model, optimizer, loss_fn
 
 
-# In[10]:
+def main():
+    df = pd.read_csv("train.csv")
+    df["label"] = LabelEncoder().fit_transform(df["label_group"].tolist())
+    X_title = df["title"].to_numpy()
+    generator = RandomTextSemiLoader(df["title"].to_numpy(),
+                                     df["label"].to_numpy(),
+                                     batch_size=params["BATCH_SIZE"],
+                                     shuffle=True)
 
+    model, optimizer, loss_fn = create_model()
+    checkpoint, checkpoint_prefix = create_checkpoint(model, optimizer)
+    train_summary_writer, val_summary_writer = create_tf_summary_writer()
 
-ids = tf.keras.layers.Input((params["MAX_LEN"],), dtype=tf.int32)
-att = tf.keras.layers.Input((params["MAX_LEN"],), dtype=tf.int32)
-tok = tf.keras.layers.Input((params["MAX_LEN"],), dtype=tf.int32)
-x1 = word_model(ids, attention_mask=att, token_type_ids=tok)[-1]
-embedding = BertLastHiddenState(multi_sample_dropout=True)(x1)
+    @tf.function
+    def train_step(x1, x2, y):
+        with tf.GradientTape() as tape:
+            X_emb1, X_emb2 = model(x1), model(x2)
 
-embedding_norm = tf.math.l2_normalize(embedding,axis=1)
-model = tf.keras.models.Model(inputs=[ids, att, tok], outputs=[embedding_norm])
+            y_pred = pairwise_dist(X_emb1, X_emb2)
+            loss_value = loss_fn(y_true=y, y_pred=y_pred)
+            del X_emb1, X_emb2
+        grads = tape.gradient(loss_value, model.trainable_weights)
+        optimizer.apply_gradients(zip(grads, model.trainable_weights))
+        return loss_value
 
-optimizer = tf.keras.optimizers.Adam()
-loss_fn = contrastive_loss
-
-model.compile(optimizer=optimizer, loss=loss_fn)
-
-
-# In[11]:
-
-
-print(model.summary())
-
-generator = RandomTextSemiLoader(df["title"].to_numpy(), df["label"].to_numpy(),batch_size=params["BATCH_SIZE"],shuffle=True)
-
-
-@tf.function
-def train_step(x1,x2, y):
-    with tf.GradientTape() as tape:
-        X_emb1 = model(x1)        
-        X_emb2 = model(x2)
+    @tf.function
+    def valid_step(x1, x2, y):
+        X_emb1, X_emb2 = model(x1), model(x2)
 
         y_pred = pairwise_dist(X_emb1, X_emb2)
-        # print("Compute prediction")
         loss_value = loss_fn(y_true=y, y_pred=y_pred)
-        
-        del X_emb1, X_emb2, y
-    grads = tape.gradient(loss_value, model.trainable_weights)
-    optimizer.apply_gradients(zip(grads, model.trainable_weights))
-        # train_acc_metric.update_state(y, logits)
-    return loss_value
 
-@tf.function
-def valid_step(x1, x2, y):
-    X_emb1 = model(x1)        
-    X_emb2 = model(x2)
+        del X_emb1, X_emb2
 
-    y_pred = pairwise_dist(X_emb1, X_emb2)
-    # print("Compute prediction")
-    loss_value = loss_fn(y_true=y, y_pred=y_pred)
-    
-    del X_emb1, X_emb2, y
+        return loss_value
 
-    return loss_value
+    for epoch in range(params["EPOCHS"]):
+        print("Start epoch {}/{} \n".format((epoch + 1), params["EPOCHS"]))
 
-for epoch in range(params["EPOCHS"]):
-    logger.info("Start epoch {}/{} \n".format((epoch+1),params["EPOCHS"]))
-    epochStart = time.time()
-    
-    cum_loss = 0.0
+        steps_per_epoch = len(generator)
+        pbar = tf.keras.utils.Progbar(steps_per_epoch)
 
-    steps_per_epoch = len(generator)
-    pbar = tf.keras.utils.Progbar(steps_per_epoch)
+        cum_loss_train, cum_loss_val = 0.0, 0.0
+        for step in range(steps_per_epoch):
+            X_idx, y = generator.get(step)
 
-    for step in range(steps_per_epoch):
-        X_idx, y = generator.__getitem__(step)        
-        
-        train_idx, val_idx, _ , _ = train_test_split(np.arange(len(X_idx)), y, test_size=0.3, random_state=4111, shuffle=True)
+            train_idx, val_idx, _, _ = train_test_split(np.arange(len(X_idx)), y, test_size=0.2, random_state=4111,
+                                                        shuffle=True)
 
-        X_1, X_2 = encoder(X_title[X_idx[:,0][train_idx]]), encoder(X_title[X_idx[:,1][train_idx]])
-        X_val_1, X_val_2 = encoder(X_title[X_idx[:,0][val_idx]]), encoder(X_title[X_idx[:,1][val_idx]])
-        y_train, y_test = y[train_idx], y[val_idx]
+            X_1, X_2 = encoder(X_title[X_idx[:, 0][train_idx]]), encoder(X_title[X_idx[:, 1][train_idx]])
+            X_val_1, X_val_2 = encoder(X_title[X_idx[:, 0][val_idx]]), encoder(X_title[X_idx[:, 1][val_idx]])
+            y_train, y_test = y[train_idx], y[val_idx]
 
-        loss_value = train_step(X_1, X_2, y_train)
-        val_loss_value = valid_step(X_val_1, X_val_2,y_test)
+            loss_value = train_step(X_1, X_2, y_train)
+            val_loss_value = valid_step(X_val_1, X_val_2, y_test)
 
-        pbar.update(step,values=[("log_loss",loss_value), ("val_loss", val_loss_value)])
+            pbar.update(step, values=[("log_loss", loss_value), ("val_loss", val_loss_value)])
 
-        del X_1, X_2, X_idx
-        gc.collect()
+            cum_loss_train += loss_value
+            cum_loss_val += val_loss_value
 
-    generator.on_epoch_end()
+            del X_1, X_2, X_idx
+            gc.collect()
 
-    epochEnd = time.time()
-    print(epochEnd - epochStart)
+        with train_summary_writer.as_default():
+            tf.summary.scalar("loss", cum_loss_train / steps_per_epoch, epoch)
 
-model.save("saved/pair-%s"%params["MODEL_NAME"])
+        with val_summary_writer.as_default():
+            tf.summary.scalar("loss", cum_loss_val / steps_per_epoch, epoch)
+
+        checkpoint.save(file_prefix=checkpoint_prefix)
+        generator.on_epoch_end()
+
+    train_summary_writer.flush()
+    val_summary_writer.flush()
+
+    model.save(model_dir)
+
+
+if __name__ == '__main__':
+    main()
