535f3b39ad794c915c5579d92e26eb70e98e2c2e
Nhuan Tran <nhuan.tran@onemount.com>
2021-05-01 11:00:19 +0700

    Resnet50 trainable

--- metric_image_effnet.py
+++ metric_image_effnet.py
@@ -1,7 +1,7 @@
 import argparse
 
 from sklearn.model_selection import KFold
-
+import efficientnet.tfkeras as efn
 from features.img import *
 from features.pool import LocalGlobalExtractor
 from modelling.metrics import MetricLearner
@@ -49,12 +49,14 @@ model_dir = os.path.join(saved_path, "saved", params["model_name"], str(params["
 os.makedirs(model_dir, exist_ok=True)
 
 image_extractor_mapper = {
-    "b0": tf.keras.applications.EfficientNetB0,
-    "b1": tf.keras.applications.EfficientNetB1,
-    "b2": tf.keras.applications.EfficientNetB2,
-    "b3": tf.keras.applications.EfficientNetB3,
-    "b4": tf.keras.applications.EfficientNetB4,
-    "b5": tf.keras.applications.EfficientNetB5
+    "b0": efn.EfficientNetB0,
+    "b1": efn.EfficientNetB1,
+    "b2": efn.EfficientNetB2,
+    "b3": efn.EfficientNetB3,
+    "b4": efn.EfficientNetB4,
+    "b5": efn.EfficientNetB5,
+    "b6": efn.EfficientNetB6,
+    "b7": efn.EfficientNetB7
 }
 
 
