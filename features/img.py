eea5565025160b75afbcd2e617fe5a1e2312951a
Nhuan Tran <nhuan.tran@onemount.com>
2021-05-09 12:23:02 +0700

    Random crop multi scale

--- features/img.py
+++ features/img.py
@@ -42,20 +42,34 @@ def normalize_image(image):
 
 def decode_image(image_data, IMAGE_SIZE=(512, 512)):
     image = tf.image.decode_jpeg(image_data, channels=3)
-    # new_seed = tf.random.experimental.stateless_split(4111, num=1)[0, :]
-    # tf.random.uniform(256,512)
     image = tf.image.resize(image, (IMAGE_SIZE[0], IMAGE_SIZE[1]))
-    image = tf.image.random_crop(image, (224,224, 3))
+    image = tf.image.random_crop(image, (224, 224, 3))
     image = normalize_image(image)
-    image = tf.reshape(image, [224,224, 3])
+    image = tf.reshape(image, [224, 224, 3])
     return image
 
 
+def decode_image_random_scale(image_data, IMAGE_SIZE=(512, 512)):
+    image = tf.image.decode_jpeg(image_data, channels=3)
+    image = iso_scale(image)
+    image = tf.image.random_crop(image, (224, 224, 3))
+    image = normalize_image(image)
+    image = tf.reshape(image, [224, 224, 3])
+    return image
+
+
+def iso_scale(img, scale_range=(256, 512)):
+    height, width, _ = img.shape
+    scale = tf.round(tf.random.uniform(shape=[], minval=scale_range[0], maxval=scale_range[1]).numpy(), 1)
+    img = tf.cond(width<=height,tf.image.resize(img, (scale, tf.round(scale * height / width))),tf.image.resize((tf.round(scale * width / height), scale)))
+    return img
+
+
 # This function parse our images and also get the target variable
-def read_labeled_tfrecord(example, image_size=(512, 512)):
+def read_labeled_tfrecord(example, decode_func, image_size=(512, 512)):
     example = tf.io.parse_single_example(example, image_feature_description)
     posting_id = example['posting_id']
-    image = decode_image(example['image'], image_size)
+    image = decode_func(example['image'], image_size)
     #     label_group = tf.one_hot(tf.cast(example['label_group'], tf.int32), depth = N_CLASSES)
     label_group = tf.cast(example['label_group'], tf.int32)
     matches = example['matches']
@@ -71,8 +85,23 @@ def load_dataset(filenames, ordered=False, image_size=(512, 512)):
     dataset = tf.data.TFRecordDataset(filenames, num_parallel_reads=AUTO)
     dataset = dataset.cache()
     dataset = dataset.with_options(ignore_order)
-    dataset = dataset.map(lambda example: read_labeled_tfrecord(example, image_size=image_size),
+    dataset = dataset.map(lambda example: read_labeled_tfrecord(example, decode_image, image_size=image_size),
                           num_parallel_calls=AUTO)
+
+    return dataset
+
+
+def load_dataset_multi_scale(filenames, ordered=False, image_size=(512, 512)):
+    ignore_order = tf.data.Options()
+    if not ordered:
+        ignore_order.experimental_deterministic = False
+
+    dataset = tf.data.TFRecordDataset(filenames, num_parallel_reads=AUTO)
+    dataset = dataset.cache()
+    dataset = dataset.with_options(ignore_order)
+    dataset = dataset.map(
+        lambda example: read_labeled_tfrecord(example, decode_image_random_scale, image_size=image_size),
+        num_parallel_calls=AUTO)
 
     return dataset
 
@@ -85,7 +114,19 @@ def get_training_dataset(filenames, batch_size, ordered=False, image_size=(512, 
     dataset = dataset.shuffle(1024)
     dataset = dataset.batch(batch_size)
     dataset = dataset.prefetch(AUTO)
+    dataset = dataset.map(lambda posting_id, image, label_group, matches: (image, label_group))
 
+    return dataset
+
+
+def get_training_dataset_multi_scale(filenames, batch_size, ordered=False, image_size=(512, 512)):
+    dataset = load_dataset_multi_scale(filenames, ordered=ordered, image_size=image_size)
+    dataset = dataset.map(data_augment, num_parallel_calls=AUTO)
+    dataset = dataset.map(arcface_format, num_parallel_calls=AUTO)
+    dataset = dataset.repeat()
+    dataset = dataset.shuffle(1024)
+    dataset = dataset.batch(batch_size)
+    dataset = dataset.prefetch(AUTO)
     dataset = dataset.map(lambda posting_id, image, label_group, matches: (image, label_group))
 
     return dataset
---
+++ metric_image_effnet_tpu_multi_scale.py
@@ -0,0 +1,149 @@
+import argparse
+
+import tensorflow_addons as tfx
+import efficientnet.tfkeras as efn
+from sklearn.model_selection import KFold
+
+from features.img import *
+from features.pool import LocalGlobalExtractor
+from modelling.metrics import MetricLearner
+from utils import *
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_name", type=str, default='effb7')
+    parser.add_argument("--epochs", type=int, default=25)
+    parser.add_argument("--batch_size", type=int, default=32)
+    parser.add_argument("--margin", type=float, default=0.3)
+    parser.add_argument("--s", type=float, default=30)
+    parser.add_argument("--pool", type=str, default="gem")
+    parser.add_argument("--dropout", type=float, default=0.5)
+    parser.add_argument("--last_hidden_states", type=int, default=3)
+    parser.add_argument("--fc_dim", type=int, default=512)
+    parser.add_argument("--lr", type=float, default=0.00001)
+    parser.add_argument("--weight_decay", type=float, default=1e-5)
+    parser.add_argument("--l2_wd", type=float, default=1e-5)
+    parser.add_argument("--metric", type=str, default="adacos")
+    parser.add_argument("--input_path", type=str)
+    parser.add_argument("--warmup_epoch", type=int, default=0)
+    parser.add_argument("--verbose", type=int, default=0)
+    parser.add_argument("--resume_fold", type=int, default=None)
+    parser.add_argument("--image_size", type=int, default=512)
+    parser.add_argument("--valid_image_size", type=int)
+    parser.add_argument("--freeze", type=bool, default=False)
+    parser.add_argument("--saved_path", type=str, default=get_disk_path())
+    parser.add_argument("--check_period", type=int, default=5)
+    parser.add_argument("--patience", type=int, default=5)
+
+    parser.add_argument("--optim", type=str, default="adam")
+    parser.add_argument("--is_checkpoint", type=bool, default=True)
+    parser.add_argument("--lr_schedule", type=str, default="")
+
+    args = parser.parse_args()
+    params = vars(args)
+    return params
+
+
+params = parse_args()
+
+SEED = 4111
+N_CLASSES = 11014
+IMAGE_SIZE = (params["image_size"], params["image_size"])
+VALID_IMAGE_SIZE = (384,384)
+
+saved_path = params["saved_path"]
+model_id = "multiscale" + "_".join([params["model_name"], str(params["image_size"]), str(params["batch_size"]), str(params["optim"]),
+                     str(params["lr_schedule"]), str(params["metric"])])
+model_dir = os.path.join(saved_path, "saved", model_id)
+os.makedirs(model_dir, exist_ok=True)
+
+image_extractor_mapper = {
+    "b0": efn.EfficientNetB0,
+    "b1": efn.EfficientNetB1,
+    "b2": efn.EfficientNetB2,
+    "b3": efn.EfficientNetB3,
+    "b4": efn.EfficientNetB4,
+    "b5": efn.EfficientNetB5,
+    "b6": efn.EfficientNetB6,
+    "b7": efn.EfficientNetB7,
+    "l2": efn.EfficientNetL2
+}
+
+
+def create_model():
+    inp = tf.keras.layers.Input(shape=(224,224, 3), name='inp1')
+    label = tf.keras.layers.Input(shape=(), dtype=tf.int32, name='inp2')
+    labels_onehot = tf.one_hot(label, depth=N_CLASSES, name="onehot")
+    effnet = image_extractor_mapper[params["model_name"]](include_top=False, weights="imagenet", )
+
+    x = effnet(inp)
+    emb = LocalGlobalExtractor(params["pool"], params["fc_dim"], params["dropout"])(x)
+
+    x1 = MetricLearner(N_CLASSES, metric=params["metric"], l2_wd=params["l2_wd"])([emb, labels_onehot])
+
+    model = tf.keras.Model(inputs=[inp, label], outputs=[x1])
+    model.summary()
+
+    emb_model = tf.keras.Model(inputs=[inp], outputs=[emb])
+
+    return model, emb_model
+
+
+def main():
+    seed_everything(SEED)
+
+    resolver = tf.distribute.cluster_resolver.TPUClusterResolver(tpu='')
+    tf.config.experimental_connect_to_cluster(resolver)
+    # This is the TPU initialization code that has to be at the beginning.
+    tf.tpu.experimental.initialize_tpu_system(resolver)
+    print("All TPU devices: ", tf.config.list_logical_devices('TPU'))
+    strategy = tf.distribute.TPUStrategy(resolver)
+
+    BATCH_SIZE_PER_TPU = params["batch_size"]
+    BATCH_SIZE = BATCH_SIZE_PER_TPU * strategy.num_replicas_in_sync
+    params["batch_size"] = BATCH_SIZE
+    print("Batch size: ", BATCH_SIZE)
+
+    print("Loading data")
+    input_paths = params['input_path']
+    files = np.array([fpath for fpath in tf.io.gfile.glob(input_paths + "/*.tfrec")])
+
+    print("Found files: ", files)
+
+    n_folds = 5
+    cv = KFold(n_folds, shuffle=True, random_state=SEED)
+    for fold_idx, (train_files, valid_files) in enumerate(cv.split(files, np.arange(n_folds))):
+        if params["resume_fold"] and params["resume_fold"] != fold_idx:
+            continue
+
+        ds_train = get_training_dataset_multi_scale(files[train_files], params["batch_size"], image_size=IMAGE_SIZE)
+        num_training_images = count_data_items(files[train_files])
+        print("Get fold %s, ds training, %s images" % (fold_idx + 1, num_training_images))
+
+        print(f'Dataset: {num_training_images} training images')
+
+        print("Get ds validation")
+        ds_val = get_validation_dataset(files[valid_files], params["batch_size"], image_size=VALID_IMAGE_SIZE)
+
+        optimizers = tfx.optimizers.AdamW(weight_decay=params["weight_decay"],
+                                          learning_rate=params["lr"])
+        if params["optim"] == "sgd":
+            optimizers = tf.optimizers.SGD(learning_rate=params["lr"], momentum=0.9, decay=1e-5)
+
+        callbacks = []
+        if params["lr_schedule"] == "cosine":
+            callbacks.append(get_cosine_annealing(params, num_training_images))
+        elif params["lr_schedule"] == "linear":
+            callbacks.append(get_linear_decay(params))
+
+        model_id = "fold_" + str(fold_idx)
+
+        print("List callbacks: %v", callbacks)
+
+        train_tpu(params, create_model, optimizers, callbacks, ds_train, ds_val,
+                  num_training_images, model_dir, model_id, strategy)
+
+
+if __name__ == "__main__":
+    main()
